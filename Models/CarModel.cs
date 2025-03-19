39dd57842f1d4a2711c1f58e194758f6ed3150e2
NORDBBFG <79658195+NORDBBFG@users.noreply.github.com>
2025-03-19 16:10:51 +0200

    Create Models/CarModel.cs

---
+++ Models/CarModel.cs
@@ -0,0 +1,9 @@
+namespace Models
+{
+    public class CarModel
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}

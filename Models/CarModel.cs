28fb0a0ef0909a8932a6c5dd60b1dcad51c867fb
NORDBBFG <nbasisty911@gmail.com>
2025-03-26 16:41:21 +0200

    Deleted unneeded files

--- Models/CarModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Models
-{
-    public class CarModel
-    {
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
-    }
-}
